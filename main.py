6c308a3fe27641eba0d5b801a9b66f9c7dca1a0a
KittenWo0f <ezhikkorzhik@gmail.com>
2022-11-26 20:32:34 +0300

    init

---
+++ main.py
@@ -0,0 +1,67 @@
+from twitch_tokens import *
+from twitchio.ext import commands
+import time
+import random
+
+class Bot(commands.Bot):
+
+    #Инициализация бота
+    def __init__(self):
+        super().__init__(token=ACCESS_TOKEN, prefix='!', initial_channels=INITIAL_CHANNELS)
+
+    #Событие готовности бота
+    async def event_ready(self):
+        print(f'Вошел как | {self.nick}')
+        print(f'Id пользователя | {self.user_id}')
+
+    #Обработка сообщений
+    async def event_message(self, message):
+        if message.echo:
+            return
+        print(f'{message.timestamp}({message.channel.name}){message.author.name}:{message.content}')
+
+        if message.author.name == 'moobot':
+            await message.channel.send(f'@{message.author.name}, мубот соси')
+        elif message.author.name == 'gufather':
+            time.sleep(1)
+            await message.channel.send('хороший бот')
+        await self.handle_commands(message)
+
+    #Команды
+    @commands.command(name='привет')
+    async def hello(self, ctx: commands.Context):
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+    @commands.command(name='пинг')
+    async def ping(self, ctx: commands.Context):
+        await ctx.send(f'Понг {ctx.author.name}!')
+
+    @commands.cooldown(rate=1, per=30, bucket=commands.Bucket.member)
+    @commands.command(name='чмок')
+    async def chmok(self, ctx: commands.Context):
+        print(ctx.kwargs)
+        if(len(ctx.chatters) == 0):
+            await ctx.send('В этом чате некого чмокнуть PoroSad')
+        else:
+            await ctx.send(f'@{ctx.author.name} чмокнул @{random.choice(tuple(ctx.chatters)).name} 😘')
+            
+    @commands.cooldown(rate=1, per=30, bucket=commands.Bucket.member)
+    @commands.command(name='чмо')
+    async def chmo(self, ctx: commands.Context):
+        if(len(ctx.chatters) == 0):
+            await ctx.send('В этом чате нет никого')
+        else:
+            await ctx.send(f'@{ctx.author.name} назвал чмом @{random.choice(tuple(ctx.chatters)).name} 🤪')
+            
+    @commands.cooldown(rate=1, per=30, bucket=commands.Bucket.member)
+    @commands.command(name='база')
+    async def baza(self, ctx: commands.Context):
+        await ctx.send(f'хуяза')
+
+    #Обработка исключений
+    async def event_command_error(self, ctx, error: Exception) -> None:
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f'Команда "{error.command.name}" на кулдауне еще {int(error.retry_after)} сек.')
+
+bot = Bot()
+bot.run()
